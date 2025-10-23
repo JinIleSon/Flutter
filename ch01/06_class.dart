19d062cdbdb076c270d18aa2c96d821cfd766a3c
sy7521012 <sy7521012@naver.com>
2025-10-23 15:03:20 +0900

    06 진행중

--- ch01/06_class.dart
+++ ch01/06_class.dart
@@ -1,20 +1,6 @@
 /*
     날짜 : 2025/10/23
     이름 : 손진일
-    내용 : 1장 Flutter 개발환경 구축
-
-    - Android Studio 설치
-    - Flutter SDK 다운로드 및 압축 해제
-    - Android Studio > plugins > flutter, dart 플러그인 설치
-
+    내용 : 
  */
 
-import 'dart:io';
-
-void main(){
-  print('Hello World!!');
-  print("Hello Dart!");
-  print("Dart Version : ${Platform.version}");
-
-
-}
