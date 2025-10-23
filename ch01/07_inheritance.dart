6948a082633e2a83992e75f042e62376c541e001
sy7521012 <sy7521012@naver.com>
2025-10-23 17:16:40 +0900

    6~9까지

--- ch01/07_inheritance.dart
+++ ch01/07_inheritance.dart
@@ -1,20 +1,80 @@
 /*
     날짜 : 2025/10/23
     이름 : 손진일
-    내용 : 1장 Flutter 개발환경 구축
-
-    - Android Studio 설치
-    - Flutter SDK 다운로드 및 압축 해제
-    - Android Studio > plugins > flutter, dart 플러그인 설치
-
+    내용 : Dart 상속
  */
 
-import 'dart:io';
+class Animal {
+  String _name;
+
+  Animal(this._name);
+
+  void speak(){
+    print('speak $_name');
+  }
+
+  void move(){
+    print('move $_name');
+  }
+}
+
+class Dog extends Animal {
+  String _bread;
+
+  Dog(String name, this._bread) : super(name);
+
+  @override
+  void speak(){
+    print('$_name($_bread) 멍멍!');
+  }
+
+  void walk(){
+    print('$_name($_bread) 산책 중...');
+  }
+}
+class Cat extends Animal {
+  String _bread;
+
+  Cat(String name, this._bread) : super(name);
+
+  @override
+  void speak(){
+    print('$_name($_bread) 야옹!');
+  }
+
+  void walk(){
+    print('$_name($_bread) 타워 오르는 중...');
+  }
+}
 
 void main(){
-  print('Hello World!!');
-  print("Hello Dart!");
-  print("Dart Version : ${Platform.version}");
 
+  // Animal 생성
+  Animal animal = Animal("동물");
+  animal.speak();
+  animal.move();
 
+  // Dog 생성
+  Dog myDog = Dog('순돌이', '푸들');
+  myDog.speak();
+  myDog.walk();
+
+  Cat myCat = Cat('나비', '고양이');
+  myCat.speak();
+  myCat.walk();
+
+  // 다형성
+  Animal a1 = Dog('바둑이', '진돗개');
+  Animal a2 = Cat('야옹이', '고양이');
+
+  a1.speak();
+  a2.speak();
+
+  if (a1 is Dog){
+    a1.walk();
+  }
+
+  if(a2 is Cat){
+    a2.walk();
+  }
 }
