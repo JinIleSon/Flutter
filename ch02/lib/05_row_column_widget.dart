2b8f73232929ad84adb6adb99d0581085a3e54a1
sy7521012 <sy7521012@naver.com>
2025-10-24 12:36:44 +0900

    ch02 05 완료?

--- ch02/lib/05_row_column_widget.dart
+++ ch02/lib/05_row_column_widget.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: '05.Row/Column Widget 실습'),
     );
   }
 }
@@ -32,16 +32,110 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Row : 위젯을 가로로 배치하는 위젯
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.blue,
+                )
+              ],
+            ),
 
-            Text('Hello Flutter!'),
+            // Column : 위젯을 세로로 배치하는 위젯
+            Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.blue,
+                )
+              ],
+            ),
+            
+            Row(
 
+              children: [
+                Column(
+                  children: [
+                    Text('벚꽃'),
+                    Image.asset(
+                        'images/flower1.jpg',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.fill,
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('코스모스'),
+                    Image.asset(
+                        'images/flower2.jpg',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.fill,
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('해바라기'),
+                    Image.asset(
+                        'images/flower3.jpg',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.fill,
+                    )
+                  ],
+                ),
+              ],
+            ),
+
+            Container(
+              width: 400,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                border: Border.all(width: 1)
+              ),
+              margin: EdgeInsets.symmetric(vertical: 10), // 위 아래 여백 10
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center, // 기본축 정렬
+                crossAxisAlignment: CrossAxisAlignment.stretch, // 교차축 정렬
+                children: [
+                  Container(width: 50, height: 100, color: Colors.red),
+                  Container(width: 50, height: 50, color: Colors.green),
+                  Container(width: 50, height: 150, color: Colors.blue),
+                ],
+              ),
+            )
           ],
-        ),
-      ),
+      )
     );
   }
 }
